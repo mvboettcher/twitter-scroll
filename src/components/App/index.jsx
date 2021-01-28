8e3923b251e5d1d97846d0bcf9cd84c53f5740eb
Max Boettcher <mvboettcher@gmail.com>
2021-01-28 16:15:13 -0500

    fetches tweets with axios and maps response data to tweet-cards

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,20 +1,50 @@
-import React, { useState } from 'react'
-import { Container } from '@material-ui/core'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Container, CircularProgress } from '@material-ui/core'
 
-import Menu from '../Menu'
-import Feed from '../Feed'
+// import Menu from '../Menu'
+// import Feed from '../Feed'
 import TweetCard from '../TweetCard'
 
 function App() {
   const [value, setValue] = useState(0)
+  const [tweets, setTweets] = useState(null)
+  const [maxId, setMaxId] = useState(null)
 
   const handleChange = (event, newValue) => {
     setValue(newValue)
   }
 
+  const getTweets = () => {
+    axios
+      .get(
+        `http://localhost:5000/dogfishbeer${maxId ? '?max_id=' + maxId : ''}`
+      )
+      .then((res) => {
+        const data = res.data
+        const maxId = data[data.length - 1].id
+
+        setTweets(data)
+        setMaxId(maxId)
+      })
+      .catch((err) => console.log(err))
+  }
+
+  useEffect(() => {
+    getTweets()
+  }, [])
+
+  if (maxId) {
+    console.log(maxId)
+  }
+
   return (
     <Container maxWidth="sm">
-      <TweetCard />
+      {tweets ? (
+        tweets.map((tweet, idx) => <TweetCard tweet={tweet} key={idx} />)
+      ) : (
+        <CircularProgress />
+      )}
       {/* <Menu value={value} handleChange={handleChange} /> */}
       {/* <Feed value={value} index={0} content="Twitter account #1" /> */}
       {/* <Feed value={value} index={1} content="Twitter account #2" /> */}
--- src/components/TweetCard/index.jsx
+++ src/components/TweetCard/index.jsx
@@ -3,51 +3,54 @@ import { makeStyles } from '@material-ui/core/styles'
 import { Card, CardContent, Paper, Typography, Link } from '@material-ui/core'
 
 import { displayPublishedDate, linkChecker } from '../../helpers'
-import data from '../../data'
 
-export default function TweetCard() {
+export default function TweetCard({ tweet }) {
   const classes = useStyles()
-  const image = data.entities.media.filter((m) => m.type === 'photo')[0]
-  const tweetBody = linkChecker(data.full_text)
-
-  console.log(tweetBody)
+  const tweetBody = linkChecker(tweet.full_text)
+  const image = tweet.entities.media
+    ? tweet.entities.media.filter((m) => m.type === 'photo')[0]
+    : null
 
   return (
     <Card className={classes.root}>
       <CardContent>
         <div className={classes.title}>
-          <Typography className={classes.userName}>{data.user.name}</Typography>
+          <Typography className={classes.userName}>
+            {tweet.user.name}
+          </Typography>
           <Typography
             variant="body2"
             color="textSecondary"
             className={classes.screenName}
           >
-            {`@${data.user.screen_name}`}
+            {`@${tweet.user.screen_name}`}
           </Typography>
           <Typography color="textSecondary" className={classes.date}>
-            {displayPublishedDate(data.created_at)}
+            {displayPublishedDate(tweet.created_at)}
           </Typography>
         </div>
         {/* DISPLAY TWEET BODY */}
-        {tweetBody.map((t, idx) => {
-          if (t.type === 'link') {
-            return (
-              <Link key={idx} href={t.text} variant="body2">
-                {t.text}
-              </Link>
-            )
-          } else {
-            return (
-              <Typography key={idx} variant="body2" component="p">
-                {t.text}
-              </Typography>
-            )
-          }
-        })}
-        {/* DISPLAY TWEET IMAGE (IF AVAILABLE) */}
+        <span>
+          {tweetBody.map((t, idx) => {
+            if (t.type === 'link') {
+              return (
+                <Link key={idx} href={t.text} variant="body2">
+                  {t.text}
+                </Link>
+              )
+            } else {
+              return (
+                <Typography key={idx} variant="body2" component="p">
+                  {t.text}
+                </Typography>
+              )
+            }
+          })}
+        </span>
+        {/* DISPLAY IMAGE (IF AVAILABLE) */}
         {image && (
           <Paper className={classes.imageContainer}>
-            <img src={image.media_url} height={200} />
+            <img src={image.media_url} height={200} alt="" />
           </Paper>
         )}
       </CardContent>
