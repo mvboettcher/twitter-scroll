e77e7b5e0e1e18269e2ab8a515f2226c5aab71c0
Max Boettcher <mvboettcher@gmail.com>
2021-01-28 21:14:30 -0500

    creates two separate feeds

--- src/components/Feed/index.jsx
+++ src/components/Feed/index.jsx
@@ -1,19 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { CircularProgress, Box } from '@material-ui/core'
+import TweetCard from '../TweetCard'
 
-const tweetsArr = Array.from(Array(101).keys())
+function Feed({ value, index, screenName }) {
+  const [tweets, setTweets] = useState([])
+  const [maxId, setMaxId] = useState(null)
 
-function Feed({ value, index, content }) {
+  const getTweets = () => {
+    axios
+      .get(
+        `http://localhost:5000/${screenName}${maxId ? '?max_id=' + maxId : ''}`
+      )
+      .then((res) => {
+        let data = res.data
+        if (maxId) {
+          data = res.data.slice(1)
+        }
+
+        const newMaxId = data[data.length - 1].id
+
+        setTweets([...tweets, ...data])
+        setMaxId(newMaxId)
+      })
+      .catch((err) => console.log(err))
+  }
+
+  useEffect(() => {
+    getTweets()
+  }, [])
+
   return (
-    <div style={{ marginTop: 60 }}>
-      {value === index && (
-        <div>
-          <h1>{content}</h1>
-          {tweetsArr.map((tweet, idx) => (
-            <h3 key={idx}>{tweet}</h3>
-          ))}
-        </div>
+    <Box
+      component="div"
+      style={{ marginBottom: 60 }}
+      display={value === index ? 'block' : 'none'}
+    >
+      {tweets ? (
+        tweets.map((tweet, idx) => <TweetCard tweet={tweet} key={idx} />)
+      ) : (
+        <CircularProgress />
       )}
-    </div>
+    </Box>
   )
 }
 
